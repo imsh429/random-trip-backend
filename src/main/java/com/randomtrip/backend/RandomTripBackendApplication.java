599d51f3c5fa786a3af24391ddc142bb2d3d4ef3
imsh429 <sh99429@naver.com>
2025-05-07 23:31:36 +0900

    Initial commit : springboot + gradle setup

---
+++ src/main/java/com/randomtrip/backend/RandomTripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.randomtrip.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomTripBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomTripBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randomtrip/backend/RandomTripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomtrip.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomTripBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
